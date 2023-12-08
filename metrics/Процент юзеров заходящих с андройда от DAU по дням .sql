aacaf1bdea8b148ce9faf49951c59d780e8106f6
Alexandr <aleksandr_sterkhov@bk.ru>
2023-12-08 16:50:24 +0300

    Update Процент юзеров заходящих с андройда от DAU по дням .sql

--- metrics/Процент юзеров заходящих с андройда от DAU по дням .sql
+++ metrics/Процент юзеров заходящих с андройда от DAU по дням .sql
@@ -18,3 +18,16 @@ from(SELECT *,
      		ROUND(uniques / sum(uniques) over (PARTITION BY log_date), 2) as dau_perc
      from dau) r
 where app_id='Android'
+
+-- Или тоже самое покороче через подзапросы
+select * 
+from(	select *, 
+		sum(dau_per_app) over(partition by log_date) dau,
+		round(dau_per_app::numeric/sum(dau_per_app) over(partition by log_date),2) dau_perc 
+	from (
+		select log_date, app_id,
+	  	       count(distinct user_id) dau_per_app
+		from events_log
+		where name='pageOpen'
+		group by log_date,app_id)a) b
+where app_id ='Android'
