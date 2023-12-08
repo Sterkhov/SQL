afa4f8e075ec31026e8e6ab601655daf6feee367
Alexandr <aleksandr_sterkhov@bk.ru>
2023-12-08 20:05:13 +0300

    Delete metrics/Task #1. DAU where pageOpen.sql

--- metrics/Task #1. DAU where pageOpen.sql
+++
@@ -1,8 +0,0 @@
-#Рассчитайте общий DAU с группировкой по дням. Чтобы запрос работал быстрее, не считайте все типы событий, а возьмите только аналитические события типа pageOpen.
-
-SELECT dt as date,
-	   COUNT(DISTINCT(user_id)) as DAU
-FROM events_log
-WHERE name = 'pageOpen'
-group by dt
-order by dt
