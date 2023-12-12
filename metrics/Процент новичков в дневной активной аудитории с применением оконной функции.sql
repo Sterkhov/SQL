c0614f46bb8af1107cd41282de0a7f89d96d6d6d
Alexandr <aleksandr_sterkhov@bk.ru>
2023-12-12 19:57:49 +0300

    Update and rename Percent of  new users in DAU with WINDOW function.sql to Процент новичков в дневной активной аудитории с применением оконной функции.sql

--- metrics/Процент новичков в дневной активной аудитории с применением оконной функции.sql
+++ metrics/Процент новичков в дневной активной аудитории с применением оконной функции.sql
@@ -1,24 +1,24 @@
 --какую долю составляют новички от общего DAU
 with 
-dau as(
+new_old_users as(
   select log_date,
          CASE
          	when log_date=install_date then 'новые пользователи'
          	else 'старые пользователи'
          END as new_old,
-  		 count(DISTINCT(user_id)) as uniques
+  		 count(DISTINCT(user_id)) users
   from events_log
   group by log_date, 
   		CASE
         	when log_date=install_date then 'новые пользователи'
             else 'старые пользователи'
         END
-order by log_date, uniques DESC),
-perc_dau as(
+order by log_date, users DESC),
+dau as(
   	select *,
-  			sum(uniques) over(PARTITION by log_date) as total,
-  			cast(uniques as float) / sum(uniques) over(PARTITION by log_date)
-    from dau)
+  			sum(users) over(PARTITION by log_date) total,
+  			round(users::numeric / sum(users) over(PARTITION by log_date), 2) perc_dau
+    from new_old_users)
 select *
-from perc_dau
+from dau
 where new_old='новые пользователи'
