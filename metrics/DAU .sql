cad734924686e17f2847e73f3d3784e149500a4c
Alexandr <aleksandr_sterkhov@bk.ru>
2023-12-15 15:24:19 +0300

    Update and rename Расчет DAU.sql to DAU .sql

---
+++ metrics/DAU .sql
@@ -0,0 +1,10 @@
+--Рассчитайте общий DAU с группировкой по дням. 
+
+
+SELECT log_date,
+		COUNT(distinct user_id) as new_users
+from events_log
+where log_date = install_date
+and name = 'pageOpen'
+group by log_date
+ORDER by log_date
