8207a2a27dee73cf7164448d26bc8400c7f3549d
Alexandr <aleksandr_sterkhov@bk.ru>
2025-05-26 01:12:08 +0300

    Delete Тестовое 01.07.23 directory

--- Тестовое 01.07.23/Задание №1. Количество самолётов вылетевших из аэропортов в мае 2017 года.sql
+++
@@ -1,7 +0,0 @@
---Сколько самолётов вылетело из аэропортов в мае 2017 года
-
-select COUNT(aircraft_code) Количество_самолетов_вылетевших_в_мае_2017
-from flights
-where DATE_TRUNC('month', actual_departure) = '2017-05-01 00:00:00+03'
-and status = 'Arrived'
-group by status
